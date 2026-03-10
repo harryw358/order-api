f2d3b501db5c2b240f1ac076df31cce084bf41ca
harryw83 <122788275+harryw83@users.noreply.github.com>
2026-03-10 17:00:25 +0000

    Created models and database

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Order(models.Model):
+    invoice_no = models.CharField(max_length=20, primary_key=True)
+    customer_id = models.CharField(max_length=50, null=True, blank=True)
+    invoice_date = models.DateTimeField()
+    total_items = models.IntegerField(default=0)
+    order_status = models.CharField(
+        max_length=50,
+        default='Pending',
+        choices=[
+            ('Pending', 'Pending'),
+            ('Ready for Collection', 'Ready for Collection'),
+            ('Collected', 'Collected'),
+            ('Cancelled', 'Cancelled')
+        ]
+    )
+
+    def __str__(self):
+        return f"Order {self.invoice_no} - {self.order_status}"
