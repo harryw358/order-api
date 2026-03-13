a418730f15203e452aaab38c9e6f4640728b64f4
harryw83 <122788275+harryw83@users.noreply.github.com>
2026-03-13 18:38:46 +0000

    Add admin page

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
 # Register your models here.
+
+# This allows you to view/edit items directly inside the parent Order page!
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('invoice_no', 'customer_id', 'country', 'invoice_date', 'order_status')
+    list_filter = ('order_status', 'country')
+    search_fields = ('invoice_no', 'customer_id')
+    inlines = [OrderItemInline]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('stock_code', 'description', 'quantity', 'price', 'order')
+    search_fields = ('stock_code', 'description')
